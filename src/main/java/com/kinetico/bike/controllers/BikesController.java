12310b91e7c0ae34d72fb32e87c7681aeae02ad2
Mandar Kathane <kathanemandar@gmail.com>
2023-12-10 01:16:16 +0530

    Code for Spring Boot DB Read Write Operation.

--- src/main/java/com/kinetico/bike/controllers/BikesController.java
+++ src/main/java/com/kinetico/bike/controllers/BikesController.java
@@ -1,6 +1,8 @@
 package com.kinetico.bike.controllers;
 
 import com.kinetico.bike.models.Bike;
+import com.kinetico.bike.repositores.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,9 @@ import java.util.List;
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
 
+    @Autowired
+    private BikeRepository bikeRepository;
+
     @GetMapping
     public String getBikeName() {
         return "Base Bike";
@@ -18,18 +23,21 @@ public class BikesController {
 
     @GetMapping("all-bikes")
     public List<Bike> list() {
-        List<Bike> bikes = new ArrayList<>();
-        return bikes;
+//        List<Bike> bikes = new ArrayList<>();
+//        return bikes;
+        return bikeRepository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike) {
+        bikeRepository.save(bike);
     }
 
     @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id) {
-        return new Bike();
+//        return new Bike();
+        return bikeRepository.getOne(id);
     }
 
 }
